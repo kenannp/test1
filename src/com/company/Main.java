0b3f279f81507278ab8d8b5d65d11d1111fdae7f
Musti <mustafa.rasheed@web.de>
2020-04-13 00:55:04 +0200

    Include Main Method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,23 @@
 package com.company;
 
+import com.company.KopfZahlCamera.Ergebnis;
+
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] lottoNumbers = new int[4];
 
-        lottoNumbers[0]=300;
+        int anzahl = 7;
 
-        System.out.println(Arrays.toString(lottoNumbers));
+        Ergebnis[] wuerfe = new Ergebnis[anzahl];
+
+        for (int i = 0; i < anzahl; i++) {
+            wuerfe[i] = KopfZahlCamera.ermittleErgebnis();
+        }
+
+        System.out.println(Arrays.toString(wuerfe));
+
 
     }
 }
