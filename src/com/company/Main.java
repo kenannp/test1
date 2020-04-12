ee57b20a3e7065734e587791f87f3794762bf5d3
Kenan <kenan.prtinac@web.de>
2020-04-12 21:09:09 +0200

    Main Methode hinzugefügt

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] lottoNumbers = new int[4];
+
+        int zahl =2;
+
+        Object o = "";
+
+        lottoNumbers[0]=300;
+
+        System.out.println(lottoNumbers);
+
+    }
+}
