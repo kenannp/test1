13ee164cafa5834d56a1b2b34cbf34409fa46b3e
Musti <mustafa.rasheed@web.de>
2020-04-12 21:36:39 +0200

    Bessere Print Ausgabe

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,9 @@ public class Main {
     public static void main(String[] args) {
         int[] lottoNumbers = new int[4];
 
-        int zahl =2;
-
-        Object o = "";
-
         lottoNumbers[0]=300;
 
-        System.out.println(lottoNumbers);
+        System.out.println(Arrays.toString(lottoNumbers));
 
     }
 }
